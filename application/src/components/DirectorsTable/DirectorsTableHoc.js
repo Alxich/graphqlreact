a6d06859924e6a196317f4b08143f9648fca0e6b
Alxich <belich.olexandr@gmail.com>
2022-12-19 20:37:00 +0200

    First commit

---
+++ application/src/components/DirectorsTable/DirectorsTableHoc.js
@@ -0,0 +1,20 @@
+import { withStyles } from "@material-ui/core/styles";
+import { compose } from "recompose";
+import { graphql } from "react-apollo";
+
+import { directorsQuery } from "./queries";
+
+import { styles } from "./styles";
+
+const withGraphql = graphql(directorsQuery, {
+  options: ({ name = "" }) => ({
+    variables: {
+      name,
+    },
+  }),
+});
+
+export default compose(
+  withStyles(styles),
+  withGraphql
+);
